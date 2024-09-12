1878a6157613ac78b4bc9a0c27cb385da273fb35
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-09-12 21:24:54 +0700

    sign in and sign up images

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -38,55 +38,76 @@ const Login = () => {
             }
         } 
 
-    return <div>
-        <section className="pl-[10rem] flex flex-wrap">
-            <div className="mr-[4rem] mt-[5rem]">
-                <h1 className="text-2xl font-semibold mb-4"> Sign In</h1>
-
-                <form onSubmit={submitHandler} className="container w-[40rem]">
+        return (
+            <div className="flex justify-between items-center h-screen">
+              <section className="pl-[5rem] flex flex-col items-center w-[50%]">
+                <div className="mt-[2rem] w-full max-w-[40rem]"> {/* Mengatur lebar maksimal form */}
+                  <h1 className="text-4xl font-semibold mb-6 text-center text-white">Sign In</h1> {/* Membesarkan ukuran font judul */}
+                  
+                  <form onSubmit={submitHandler} className="w-full">
                     <div className="my-[2rem]">
-                        <label htmlFor="email" className="block text-sm font-medium text-white">
-                            Email Address</label>
-                        <input type="email"
-                            id="email"
-                            className="mt-1 p-2 border rounded w-full"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
+                      <label
+                        htmlFor="email"
+                        className="block text- font-medium text-white"
+                      >
+                        Email Address
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        className="mt-1 p-4 border rounded w-full text-lg"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
                     </div>
-
+          
                     <div className="my-[2rem]">
-                        <label htmlFor="password" className="block text-sm font-medium text-white">
-                            Password</label>
-                        <input type="password"
-                            id="password"
-                            className="mt-1 p-2 border rounded w-full"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
+                      <label
+                        htmlFor="password"
+                        className="block text-1xl font-medium text-white"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        id="password"
+                        className="mt-1 p-4 border rounded w-full text-lg"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
                     </div>
-
-                    <button disabled={isLoading}
-                        type="submit"
-                        className="bg-orange-600 
-                            text-white px-4 
-                            py-2 rounded 
-                            cursor-pointer my-[1rem]"
-                    > {isLoading ? "Signing..." : "Sign In"}
+          
+                    <button
+                      disabled={isLoading}
+                      type="submit"
+                      className="bg-orange-600 text-white px-6 py-3 rounded cursor-pointer my-[1rem] w-full text-lg"
+                    >
+                      {isLoading ? "Signing..." : "Sign In"}
                     </button>
                     {isLoading && <Loader />}
-                </form>
-
-                <div className="mt-4">
+                  </form>
+          
+                  <div className="mt-4 text-center">
                     <p className="text-white">
-                        New Customer ? {" "}
-                        <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}
-                        className="text-orange-600 hover:underline"> Register
-                        </Link>
+                      New Customer?{" "}
+                      <Link
+                        to={redirect ? `/register?redirect=${redirect}` : "/register"}
+                        className="text-orange-600 hover:underline"
+                      >
+                        Register
+                      </Link>
                     </p>
+                  </div>
                 </div>
+              </section>
+          
+              <img
+                src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+                alt="Sign In Background"
+                className="h-[65rem] w-[50%] xl:block md:hidden sm:hidden rounded-lg object-cover"
+              />
             </div>
-        </section>
-    </div>
+          )
+          
 }
 export default Login
--- frontend/src/pages/auth/Navigation.jsx
+++ frontend/src/pages/auth/Navigation.jsx
@@ -1,214 +1,215 @@
-import { useState } from "react"
-import {
-  AiOutlineHome,
-  AiOutlineShopping,
-  AiOutlineLogin,
-  AiOutlineUserAdd,
-  AiOutlineShoppingCart,
-} from "react-icons/ai"
-import { FaHeart } from "react-icons/fa"
-import { Link, useNavigate } from "react-router-dom"
-import "./Navigation.css"
-import { useSelector, useDispatch } from "react-redux"
-import { useLogoutMutation } from "../../redux/api/usersApiSlice"
-import { logout } from '../../redux/features/auth/authSlice'
+      import { useState } from "react"
+      import {
+        AiOutlineHome,
+        AiOutlineShopping,
+        AiOutlineLogin,
+        AiOutlineUserAdd,
+        AiOutlineShoppingCart,
+      } from "react-icons/ai"
+      import { FaHeart } from "react-icons/fa"
+      import { Link, useNavigate } from "react-router-dom"
+      import "./Navigation.css"
+      import { useSelector, useDispatch } from "react-redux"
+      import { useLogoutMutation } from "../../redux/api/usersApiSlice"
+      import { logout } from '../../redux/features/auth/authSlice'
 
-const Navigation = () => {
-  const { userInfo } = useSelector(state => state.auth)
-  console.log("userInfo:", userInfo)
-  const [dropdownOpen, setDropdownOpen] = useState(false)
-  const [showSidebar, setShowSidebar] = useState(false)
+      const Navigation = () => {
+        const { userInfo } = useSelector(state => state.auth)
+        console.log("userInfo:", userInfo)
+        const [dropdownOpen, setDropdownOpen] = useState(false)
+        const [showSidebar, setShowSidebar] = useState(false)
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen)
-  }
+        const toggleDropdown = () => {
+          setDropdownOpen(!dropdownOpen)
+        }
 
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar) // Diperbaiki dari setDropdownOpen
-  }
+        const toggleSidebar = () => {
+          setShowSidebar(!showSidebar) // Diperbaiki dari setDropdownOpen
+        }
 
-  const closeSidebar = () => {
-    setShowSidebar(false)
-  }
+        const closeSidebar = () => {
+          setShowSidebar(false)
+        }
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+        const dispatch = useDispatch()
+        const navigate = useNavigate()
 
-  const [logoutApiCall] = useLogoutMutation()
+        const [logoutApiCall] = useLogoutMutation()
 
-  const logoutHandler = async () => {
-    try {
-      await logoutApiCall().unwrap()
-      dispatch(logout())
-      navigate("/login")
-    } catch (error) {
-      console.error(error)
-    }
-  }
+        const logoutHandler = async () => {
+          try {
+            await logoutApiCall().unwrap()
+            dispatch(logout())
+            navigate("/login")
+          } catch (error) {
+            console.error(error)
+          }
+        }
 
-  return (
-    <div
-      style={{ zIndex: 9999 }}
-      className={`${
-        showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg w-[4%] hover:w-[15%] h-[100vh]  fixed `}
-      id="navigation-container"
-    >
-      <div className="flex flex-col justify-center space-y-4">
-        <Link
-          to="/"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]"> Home </span>{" "}
-        </Link>
-
-        <Link
-          to="/shop"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]"> Shopping </span>{" "}
-        </Link>
-
-        <Link
-          to="/cart"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]"> Cart </span>{" "}
-        </Link>
-
-        <Link
-          to="/favorits"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <FaHeart className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]"> Favorites </span>{" "}
-        </Link>
-      </div>
-
-      <div className="relative">
-        <button
-          onClick={toggleDropdown}
-          className="flex items-center text-gray-800 focus:outline-none"
-        >
-          {userInfo ? (
-            <span className="text-white">{userInfo.user.username}</span>
-          ) : (
-            <></>
-          )}
-          {userInfo && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`h-4 w-4 ml-1 
-                ${dropdownOpen ? "transform rotate-180" : ""
-                }`}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
-              />
-            </svg>
-          )}
-        </button>
-
-        {dropdownOpen && userInfo && (
-          <ul
-            className={`absolute right-0 mt-2 mr-14 space-y-2 bg-white text-gray-600 
-              ${!userInfo.user.isAdmin ? "-top-20" : "-top-80"
-              } `}
+        return (
+          <div
+            style={{ zIndex: 9999 }}
+            className={`${showSidebar ? "hidden" : "flex"
+              } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+            id="navigation-container"
           >
-            {userInfo.user.isAdmin && (
-              <>
-                <li>
-                  <Link
-                    to="/admin/dashboard"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Dashboard
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/productlist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Products
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/categorylist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Category
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/orderlist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Orders
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/userlist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Users
-                  </Link>
-                </li>
-              </>
-            )}
-
-            <li>
-              <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
-                Profile
+            <div className="flex flex-col justify-center space-y-4">
+              <Link
+                to="/"
+                className="flex items-center transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+                <span className="hidden nav-item-name mt-[3rem]"> Home </span>{" "}
               </Link>
-            </li>
-            <li>
+
+              <Link
+                to="/shop"
+                className="flex items-center transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+                <span className="hidden nav-item-name mt-[3rem]"> Shopping </span>{" "}
+              </Link>
+
+              <Link
+                to="/cart"
+                className="flex items-center transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
+                <span className="hidden nav-item-name mt-[3rem]"> Cart </span>{" "}
+              </Link>
+
+              <Link
+                to="/favorits"
+                className="flex items-center transition-transform transform hover:translate-x-2"
+              >
+                <FaHeart className="mr-2 mt-[3rem]" size={26} />
+                <span className="hidden nav-item-name mt-[3rem]"> Favorites </span>{" "}
+              </Link>
+            </div>
+
+            <div className="relative">
               <button
-                onClick={logoutHandler}
-                className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+                onClick={toggleDropdown}
+                className="flex items-center text-gray-800 focus:outline-none"
               >
-                Logout
+                {userInfo ? (
+                  <span className="text-white">{userInfo.user.username}</span>
+                ) : (
+                  <></>
+                )}
+                {userInfo && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={`h-4 w-4 ml-1 
+                      ${dropdownOpen ? "transform rotate-180" : ""
+                      }`}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+                    />
+                  </svg>
+                )}
               </button>
-            </li>
-          </ul>
-        )}
-        {!userInfo && (
-          <ul>
-            <li>
-              <Link
-                to="/login"
-                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
-              >
-                <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
-                <span className="hidden nav-item-name">LOGIN</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/register"
-                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
-              >
-                <AiOutlineUserAdd size={26} />
-                <span className="hidden nav-item-name">REGISTER</span>
-              </Link>
-            </li>
-          </ul>
-        )}
-      </div>
-    </div>
-  );
-};
 
-export default Navigation;
+              {dropdownOpen && userInfo && (
+                <ul
+                  className={`absolute right-0 mt-2 mr-14 space-y-2 bg-gray-600 text-white
+                    ${!userInfo.user.isAdmin ? "-top-20" : "-top-80"}
+                    ${dropdownOpen ? 'clip-path: inset(0% 0% 0% 0%)' : 'clip-path: inset(100% 0% 0% 0%)'}
+                    `}
+                    onMouseLeave={() => setDropdownOpen(false)}
+                >
+                  {userInfo.user.isAdmin && (
+                    <>
+                      <li>
+                        <Link
+                          to="/admin/dashboard"
+                          className="block px-4 py-2 hover:bg-gray-700"
+                        >
+                          Dashboard
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/admin/productlist"
+                          className="block px-4 py-2 hover:bg-gray-700"
+                        >
+                          Products
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/admin/categorylist"
+                          className="block px-4 py-2 hover:bg-gray-700"
+                        >
+                          Category
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/admin/orderlist"
+                          className="block px-4 py-2 hover:bg-gray-700"
+                        >
+                          Orders
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/admin/userlist"
+                          className="block px-4 py-2 hover:bg-gray-700"
+                        >
+                          Users
+                        </Link>
+                      </li>
+                    </>
+                  )}
+
+                  <li>
+                    <Link to="/profile" className="block px-4 py-2 hover:bg-gray-700">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <button
+                      onClick={logoutHandler}
+                      className="block w-full px-4 py-2 text-left hover:bg-gray-700"
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              )}
+              {!userInfo && (
+                <ul>
+                  <li>
+                    <Link
+                      to="/login"
+                      className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+                    >
+                      <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
+                      <span className="hidden nav-item-name">LOGIN</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/register"
+                      className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+                    >
+                      <AiOutlineUserAdd size={26} />
+                      <span className="hidden nav-item-name">REGISTER</span>
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          </div>
+        );
+      };
+
+      export default Navigation;
