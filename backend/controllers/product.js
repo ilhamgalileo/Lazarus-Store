55e05d9b0feb0e1443238cd97138b32d37f9a171
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-09-23 16:03:58 +0700

    getall with keyword

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -2,11 +2,25 @@ const Product = require('../models/product')
 const asyncHandler = require('express-async-handler')
 
 exports.FindMany = asyncHandler(async (req, res) => {
-    const data = await Product.find()
-    if (data.length === 0) {
-        return res.status(404).send({ message: 'produk kosong' })
+    try {
+        const pageSize = 6
+        const keyword = req.query.keyword
+            ? { name: { $regex: req.query.keyword, $options: "i" } }
+            : {}
+        const count = await Product.countDocuments({...keyword})
+        const products = await Product.find({...keyword}).limit(pageSize)
+
+        res.json({
+            products,
+            page: 1,
+            pages: Math.ceil(count / pageSize),
+            hasMore: false,
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: "server error"})
     }
-    res.send(data)
 })
 
 exports.findOne = asyncHandler(async (req, res) => {
@@ -47,30 +61,30 @@ exports.create = asyncHandler(async (req, res) => {
 })
 
 exports.update = asyncHandler(async (req, res) => {
-        try {
-            const { name, brand, quantity, category, description, price, image } = req.fields
-            switch (true) {
-                case !name:
-                    return res.json({ error: "Name is required" })
-                case !brand:
-                    return res.json({ error: "Brand is required" })
-                case !quantity:
-                    return res.json({ error: "Quantity is required" })
-                case !category:
-                    return res.json({ error: "Category is required" })
-                case !description:
-                    return res.json({ error: "Description is required" })
-                case !price:
-                    return res.json({ error: "Price is required" })
-                case !image:
-                    return res.json({ error: "Image is required" })
-            }
-            const product = await Product.findByIdAndUpdate(req.params.id, { ...req.fields }, {new: true})
-            await product.save()
-            res.status(201).json({ message: 'Data Berhasil diperbarui', product })
-        } catch (error) {
+    try {
+        const { name, brand, quantity, category, description, price, image } = req.fields
+        switch (true) {
+            case !name:
+                return res.json({ error: "Name is required" })
+            case !brand:
+                return res.json({ error: "Brand is required" })
+            case !quantity:
+                return res.json({ error: "Quantity is required" })
+            case !category:
+                return res.json({ error: "Category is required" })
+            case !description:
+                return res.json({ error: "Description is required" })
+            case !price:
+                return res.json({ error: "Price is required" })
+            case !image:
+                return res.json({ error: "Image is required" })
         }
-    })
+        const product = await Product.findByIdAndUpdate(req.params.id, { ...req.fields }, { new: true })
+        await product.save()
+        res.status(201).json({ message: 'Data Berhasil diperbarui', product })
+    } catch (error) {
+    }
+})
 
 
 exports.delete = asyncHandler(async (req, res) => {
