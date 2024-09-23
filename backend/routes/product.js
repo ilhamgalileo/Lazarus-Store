90a14d98ccb3b2846ece0504d452867466fd875a
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-09-23 15:16:10 +0700

    findall dan findone controller product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,10 +5,10 @@ const { authenticate, authorizeAdmin } = require('../middlewares/middleware')
 const checkId = require('../middlewares/checkId')
 const formidable = require('express-formidable')
 
-router.get('/', product.FindMany)           
-router.get('/:id', product.findOne)         
-router.post('/', authenticate, authorizeAdmin, formidable(), product.create)           
-router.put('/:id', authenticate, authorizeAdmin,formidable(), product.update)        
+router.get('/', authenticate, authorizeAdmin, product.FindMany)
+router.get('/:id', authenticate, authorizeAdmin, product.findOne)
+router.post('/', authenticate, authorizeAdmin, formidable(), product.create)
+router.put('/:id', authenticate, authorizeAdmin, formidable(), product.update)
 router.delete('/:id', authenticate, authorizeAdmin, product.delete)
 
 module.exports = router
