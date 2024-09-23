304c59f10b82d41babb753b8ec5f459bb8824240
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-09-23 11:35:10 +0700

    products model update

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema
 
-const productSchema = new mongoose.Schema({
-    id:          { type: String, required: true },
-    name:        { type: String, required: true },
-    brand:       { type: String, required: true },
-    price:       { type: Number, required: true },
-    description: { type: String},
-    stock:       { type: Number, required: true }
+const reviewSchema = mongoose.Schema({
+    name: { type: String, require: true },
+    rating: { type: Number, require: true },
+    comment: { type: String, require: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: "User",
+    }
+}, {
+    timestamps: true
+})
+
+const productSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    image: { type: String, require: true },
+    brand: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: ObjectId, ref: "Category", required: true },
+    description: { type: String, required: true },
+    reviews: [reviewSchema],
+    rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, required: true, default: 0 }
 }, {
     timestamps: true
 })
