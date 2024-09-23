d99216bd9aa8ac99a16e762af083344671c29370
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-09-23 11:40:19 +0700

    checkId middleware

---
+++ backend/middlewares/checkId.js
@@ -0,0 +1,9 @@
+const { isValidObjectId } = require("mongoose");
+function checkId(req, res, next) {
+    if (!isValidObjectId(req.params.id)) {
+        res.status(404)
+        throw new Error("Invalid objectId")
+    }
+    next()
+}
+export default checkId
