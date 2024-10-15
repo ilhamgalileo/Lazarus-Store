e460950fd12b0b8e686d1657964fdc783fd964e6
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-10-15 12:01:15 +0700

    order controller completed

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -23,70 +23,128 @@ function calcPrice(orderItems) {
 }
 
 export const createOrder = asyncHandler(async (req, res) => {
-    try {
-        const { orderItems, shippingAddress, paymentMethod } = req.body
+    const { orderItems, shippingAddress, paymentMethod } = req.body
 
-        if (orderItems && orderItems.length === 0) {
-            res.status(400)
-            throw new Error("no order items")
-        }
-
-        const itemsFromDB = await Product.find({
-            _id: { $in: orderItems.map((x) => x._id) },
-        })
-
-        const dbOrderItems = orderItems.map((itemsFromClient) => {
-            const matchingItemFromDB = itemsFromDB.find((itemsFromDB) => itemsFromDB._id.toString() ===
-                itemsFromClient._id)
-
-            if (!matchingItemFromDB) {
-                res.status(404)
-                throw new Error(`Product not found: ${itemFromClient._id}`)
-            }
-
-            return {
-                ...itemsFromClient,
-                product: itemsFromClient._id,
-                price: matchingItemFromDB.price,
-                _id: undefined,
-            }
-        })
-        const { itemsPrice, taxPrice, shippingPrice, totalPrice } = calcPrice(dbOrderItems)
-
-        const order = new Order({
-            orderItems: dbOrderItems,
-            user: req.user._id,
-            shippingAddress,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingPrice,
-            totalPrice
-        })
-        const createOrder = await order.save()
-        res.status(201).json(createOrder)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+    if (orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error("no order items")
     }
 
+    const itemsFromDB = await Product.find({
+        _id: { $in: orderItems.map((x) => x._id) },
+    })
+
+    const dbOrderItems = orderItems.map((itemsFromClient) => {
+        const matchingItemFromDB = itemsFromDB.find((itemsFromDB) => itemsFromDB._id.toString() ===
+            itemsFromClient._id)
+
+        if (!matchingItemFromDB) {
+            res.status(404)
+            throw new Error(`Product not found: ${itemFromClient._id}`)
+        }
+
+        return {
+            ...itemsFromClient,
+            product: itemsFromClient._id,
+            price: matchingItemFromDB.price,
+            _id: undefined,
+        }
+    })
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } = calcPrice(dbOrderItems)
+
+    const order = new Order({
+        orderItems: dbOrderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice
+    })
+    const createOrder = await order.save()
+    res.status(201).json(createOrder)
 })
 
 export const getAllOrder = asyncHandler(async (req, res) => {
-    try {
-        const orders = await Order.find({}).populate("user", "id username")
-        res.json(orders)
-
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+    const orders = await Order.find({}).populate("user", "id username")
+    res.json(orders)
 })
 
 export const getMyOrder = asyncHandler(async (req, res) => {
-    try {
-        const id = req.user._id
-        const myOder = await Order.find({ user: id })
-        res.json(myOder)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+    const id = req.user._id
+    const myOder = await Order.find({ user: id })
+    res.json(myOder)
+})
+
+export const countTotalOrders = asyncHandler(async (req, res) => {
+    const totalOrders = await Order.countDocuments()
+    res.json({ totalOrders })
+})
+
+export const calcTotalSales = asyncHandler(async (req, res) => {
+    const orders = await Order.find()
+    const totalSales = orders.reduce((sum, order) => sum + order.totalPrice, 0)
+    res.json({ totalSales })
+})
+
+export const calcTotalSalesByDate = asyncHandler(async (req, res) => {
+    const salesByDate = await Order.aggregate([
+        {
+            $match: {
+                isPaid: true,
+            },
+        },
+        {
+            $group: {
+                _id: {
+                    $dateToString: { format: '$Y-$M-%d', date: '$paidAt' }
+                },
+                totalSales: { $sum: '$totalSales' }
+            },
+        },
+    ])
+    res.json(salesByDate)
+})
+
+export const findOrderById = asyncHandler(async (req, res) => {
+    const id = req.params.id
+    const order = await Order.findById(id).populate("user", "username email")
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404).json("order not found")
+    }
+})
+
+export const markOrderIsPay = asyncHandler(async (req, res) => {
+    const id = req.params.id
+    const order = await Order.findById(id)
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+        const updateOrder = await order.save()
+        res.status(200).json(updateOrder)
+    }
+})
+
+export const markOrderIsDeliver = asyncHandler(async (req, res) => {
+    const id = req.params.id
+
+    const order = await Order.findById(id)
+    if (order) {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
+    } else {
+        res.status(404).json('order not found')
     }
 })
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,16 @@
-import express  from 'express'
+import express from 'express'
 const router = express.Router()
 import { authenticate, authorizeAdmin } from '../middlewares/middleware.js'
-import * as order  from '../controllers/order.js'
- 
+import * as order from '../controllers/order.js'
+
 router.post('/checkout', authenticate, order.createOrder)
 router.get("/", authenticate, authorizeAdmin, order.getAllOrder)
 router.get("/mine", authenticate, order.getMyOrder)
+router.get('/total-orders', authenticate, order.countTotalOrders)
+router.get('/total-sales', authenticate, order.calcTotalSales)
+router.get('/total-sales-by-date', authenticate, order.calcTotalSalesByDate)
+router.get('/:id', authenticate, order.findOrderById)
+router.put('/:id/pay', authenticate, authorizeAdmin, order.markOrderIsPay)
+router.put('/:id/deliver', authenticate, authorizeAdmin, order.markOrderIsDeliver)
 
 export default router
