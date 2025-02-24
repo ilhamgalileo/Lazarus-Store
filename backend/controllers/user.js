7ceb4efd1c73870030e5621c73dee33b1fbb16e8
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2025-02-24 10:40:25 +0700

    mark user as admin backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,190 +1,215 @@
-import User from '../models/user.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
 
 export const register = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'Email existed, please create another',
-        })
-    }
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email existed, please create another",
+    });
+  }
 
-    const user = new User({ username, email, password });
-    await user.save();
+  const user = new User({ username, email, password });
+  await user.save();
 
-    res.status(201).json({
-        status: 'success',
-        message: 'Registration Successfully',
-        user: {
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-        },
-    })
-})
+  res.status(201).json({
+    status: "success",
+    message: "Registration Successfully",
+    user: {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+});
 
 export const login = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select('+password');
-    if (!user) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'Email or password not valid',
-        })
-    }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email or password not valid",
+    });
+  }
 
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (!isPasswordMatch) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'Email or password not valid',
-        })
-    }
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email or password not valid",
+    });
+  }
 
-    const token = jwt.sign(
-        {
-            _id: user._id,
-            name: user.username,
-            email: user.email,
-            superAdmin: user.superAdmin,
-            isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: '7d' }
-    )
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      name: user.username,
+      email: user.email,
+      superAdmin: user.superAdmin,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 
-    res.cookie('authToken', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    })
+  res.cookie("authToken", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
 
-    res.status(200).json({
-        status: 'success',
-        message: 'Login successfully',
-        token,
-        user: {
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-            superAdmin: user.superAdmin,
-            isAdmin: user.isAdmin,
-        },
-    })
-})
+  res.status(200).json({
+    status: "success",
+    message: "Login successfully",
+    token,
+    user: {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      superAdmin: user.superAdmin,
+      isAdmin: user.isAdmin,
+    },
+  });
+});
 
 export const logout = asyncHandler(async (req, res) => {
-    res.cookie('jwt', '', {
-        httpOnly: true,
-        expires: new Date(0),
-    });
-    res.status(200).json({ message: "Logout successfully" })
-})
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logout successfully" });
+});
 
 export const getAllUsers = asyncHandler(async (req, res) => {
-    const users = await User.find({})
-    res.json(users)
-})
+  const users = await User.find({});
+  res.json(users);
+});
 
 export const getUserCount = asyncHandler(async (req, res) => {
-    const userCount = await User.countDocuments({
-        isAdmin: null,
-    })
-    res.status(200).json({
-        userCount,
-    })
-})
+  const userCount = await User.countDocuments({
+    isAdmin: null,
+  });
+  res.status(200).json({
+    userCount,
+  });
+});
 
 export const getUserById = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        res.json({ user });
-    } else {
-        res.status(404).json({ message: 'User not found' });
-    }
-})
+  const user = await User.findById(req.params.id);
+  if (user) {
+    res.json({ user });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
 
 export const updateUserById = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id)
+  const user = await User.findById(req.params.id);
 
-    if (user) {
-        user.username = req.body.username || user.username
-        user.email = req.body.email || user.email
-        user.isAdmin = Boolean(req.body.isAdmin)
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
 
-        const updatedUser = await user.save()
+    const updatedUser = await user.save();
 
-        res.json({
-            _id: updatedUser._id,
-            username: updatedUser.username,
-            email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin,
-        })
-    } else {
-        res.status(404)
-        throw new Error("User not found")
-    }
-})
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
+  const user = await User.findById(req.user._id);
 
-    if (user) {
-        res.json({
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-        })
-    } else {
-        res.status(404).send("User not found");
-    }
-})
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404).send("User not found");
+  }
+});
 
 export const updateProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
+  const user = await User.findById(req.user._id);
 
-    if (user) {
-        user.username = req.body.username || user.username;
-        user.email = req.body.email || user.email
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
 
-        if (req.body.password) {
-            user.password = req.body.password
-        }
-        const updatedUser = await user.save()
-
-        res.json({
-            user: {
-                _id: updatedUser._id,
-                username: updatedUser.username,
-                email: updatedUser.email,
-            },
-        })
-    } else {
-        res.status(404).json({ message: "User not found" });
+    if (req.body.password) {
+      user.password = req.body.password;
     }
-})
+    const updatedUser = await user.save();
+
+    res.json({
+      user: {
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+      },
+    });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
 
 export const deleteUserByAdmin = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.clearCookie("token", {
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production",
-    })
+  const user = await User.findById(req.params.id);
+  res.clearCookie("token", {
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  });
 
-    if (user) {
-        if (user.isAdmin) {
-            res.status(400).json({ message: "Can't delete an admin account" })
-        }
-        await User.findOneAndDelete({ _id: user._id })
-        res.json({ message: 'Berhasil menghapus akun' })
-    } else {
-        res.status(404).json({ message: 'User not found' })
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400).json({ message: "Can't delete an admin account" });
     }
-})
+    await User.findOneAndDelete({ _id: user._id });
+    res.status(200).json({ message: "Delete Account successfully" });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+export const makeUserAsAdmin = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(403).json({ message: "User is admin already" });
+    }
+    user.isAdmin = true;
+    await user.save();
+  }
+  res.status(200).json({
+    message: "User has been promoted to admin",
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+  });
+});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,24 +4,22 @@ import * as user from '../controllers/user.js'
 import { authenticate, authorizeAdmin, superAdminAuth } from '../middlewares/middleware.js'
 
 router.get('/', authenticate, superAdminAuth, user.getAllUsers)
+router.put('/:id/as-admin', authenticate, superAdminAuth, user.makeUserAsAdmin)
 router.get('/count', authenticate, authorizeAdmin, user.getUserCount)
 
 router.post('/auth', user.login)
 router.post('/logout', authenticate, user.logout)
 router.post('/register', user.register)
 
-
 router
   .route('/profile')
   .get(authenticate, user.getUserProfile)
   .put(authenticate, user.updateProfile)
-
 
 router
   .route('/:id')
   .get(authenticate, authorizeAdmin, user.getUserById)
   .put(authenticate, authorizeAdmin, user.updateUserById)
   .delete(authenticate, authorizeAdmin, user.deleteUserByAdmin)
-  
 
 export default router
